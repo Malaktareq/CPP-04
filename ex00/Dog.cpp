f530262a6848b3033d838c7290220e19f56ac912
malak <you@example.com>
2025-11-14 23:23:08 +0300

    done ex00

---
+++ ex00/Dog.cpp
@@ -0,0 +1,34 @@
+#include "Dog.hpp"
+
+Dog::Dog() 
+{
+    type = "Dog";
+    std::cout << "Dog default constructor called" << std::endl;
+}
+
+Dog::Dog(const Dog &other) : Animal(other)
+{    
+    type = other.type;
+    std::cout << "Dog copy constructor called" << std::endl;
+}
+
+Dog& Dog::operator=(const Dog &other)
+{
+    if (this != &other)
+    {
+        Animal::operator=(other);
+        type = other.type;
+    }
+    std::cout << "Dog assignment operator called" << std::endl;
+    return *this;
+}
+
+Dog::~Dog()
+{
+    std::cout << "Dog destructor called" << std::endl;
+}
+
+void Dog::makeSound() const
+{
+    std::cout << "Woof Woof" << std::endl;
+}
---
+++ ex00/main.cpp
@@ -0,0 +1,36 @@
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongCat.hpp"
+int main()
+{
+    const Animal* meta = new Animal();
+    const WrongCat j =WrongCat();
+    const Animal* i = new Cat();
+    const WrongAnimal* wrongMeta = new WrongCat();
+
+    std::cout << j.getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound(); //will output the cat sound!
+    j.makeSound();
+    meta->makeSound();    
+    wrongMeta->makeSound(); // will output the wrong cat sound!
+
+    delete meta;
+    delete i;
+    delete wrongMeta;
+
+    Animal* animals[4] = {
+        new Dog(),
+        new Cat(),
+        new Dog(),
+        new Dog()
+    };
+    
+    for (int i = 0; i < 4; i++)
+        animals[i]->makeSound(); 
+    
+    for (int i = 0; i < 4; i++)
+        delete animals[i];
+
+    return 0;
+}
